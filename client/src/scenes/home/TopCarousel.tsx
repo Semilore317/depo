47b62aa5d80f7be7dfe298a0abdb5fbe73eab49d
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-04-17 18:41:52 +0100

    let me cook

--- client/src/scenes/home/TopCarousel.tsx
+++ client/src/scenes/home/TopCarousel.tsx
@@ -7,97 +7,100 @@ import { shades } from "@/theme";
 
 // Define the type for the imported images
 interface HeroTextureImports {
-  [key: string]: { default: string };
+    [key: string]: { default: string };
 }
 
 // Import all images from assets folder
 export const heroTextureImports: HeroTextureImports = import.meta.glob(
-  '../../assets/*.{png,jpg,jpeg,svg}',
-  { eager: true }
+    '../../assets/*.{png,jpg,jpeg,svg}',
+    { eager: true }
 ) as HeroTextureImports;
 
 const TopCarousel = () => {
-  const isNonMobile = useMediaQuery("(min-width: 600px)");
+    const isNonMobile = useMediaQuery("(min-width: 600px)");
 
-  return (
-    <Carousel
-      infiniteLoop={true}
-      showThumbs={false}
-      showStatus={false}
-      showIndicators={false}
-      renderArrowPrev={(onClickHandler: () => void, hasPrev: boolean, labelPrev: string) => (
-        <IconButton
-          onClick={onClickHandler}
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "0",
-            color: "white",
-            padding: "5px",
-            zIndex: 10,
-          }}
+    return (
+        <Carousel
+            autoPlay={true}
+            interval={3000}
+            stopOnHover={false}
+            infiniteLoop={true}
+            showThumbs={false}
+            showStatus={false}
+            showIndicators={true}
+            renderArrowPrev={(onClickHandler: () => void, hasPrev: boolean, labelPrev: string) => (
+                <IconButton
+                    onClick={onClickHandler}
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "0",
+                        color: "white",
+                        padding: "5px",
+                        zIndex: 10,
+                    }}
+                >
+                    <NavigateBeforeIcon sx={{ fontSize: "40px" }} />
+                </IconButton>
+            )}
+            renderArrowNext={(onClickHandler: () => void, hasNext: boolean, labelNext: string) => (
+                <IconButton
+                    onClick={onClickHandler}
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        right: "0",
+                        color: "white",
+                        padding: "5px",
+                        zIndex: 10,
+                    }}
+                >
+                    <NavigateNextIcon sx={{ fontSize: "40px" }} />
+                </IconButton>
+            )}
         >
-          <NavigateBeforeIcon sx={{ fontSize: "40px" }} />
-        </IconButton>
-      )}
-      renderArrowNext={(onClickHandler: () => void, hasNext: boolean, labelNext: string) => (
-        <IconButton
-          onClick={onClickHandler}
-          sx={{
-            position: "absolute",
-            top: "50%",
-            right: "0",
-            color: "white",
-            padding: "5px",
-            zIndex: 10,
-          }}
-        >
-          <NavigateNextIcon sx={{ fontSize: "40px" }} />
-        </IconButton>
-      )}
-    >
-      {Object.entries(heroTextureImports).map(([path, module], index) => (
-        <Box
-          key={`carousel-image-${index}`}
-          width="100%"
-          height={isNonMobile ? "600px" : "400px"}
-          sx={{
-            backgroundColor: shades.primary[500],
-            backgroundImage: module.default ? `url(${module.default})` : "none",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Box
-            color="white"
-            padding="20px"
-            borderRadius="1px"
-            textAlign="center"
-            backgroundColor="rgba(0,0,0,0.5)"
-            position="absolute"
-            top="46%"
-            left={isNonMobile ? "10%" : "0%"}
-            right={isNonMobile ? undefined : "0%"}
-            margin={isNonMobile ? undefined : "0 auto"}
-            maxWidth={isNonMobile ? undefined : "240px"}
-          >
-            <Typography color={shades.secondary[200]}>NEW ITEMS</Typography>
-            <Typography variant="h1">Summer Sale</Typography>
-            <Typography
-              fontWeight="bold"
-              color={shades.secondary[300]}
-              sx={{ textDecoration: "underline" }}
-            >
-              Discover More
-            </Typography>
-          </Box>
-        </Box>
-      ))}
-    </Carousel>
-  );
+            {Object.entries(heroTextureImports).map(([path, module], index) => (
+                <Box
+                    key={`carousel-image-${index}`}
+                    width="100%"
+                    height={isNonMobile ? "600px" : "400px"}
+                    sx={{
+                        backgroundColor: shades.primary[500],
+                        backgroundImage: module.default ? `url(${module.default})` : "none",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <Box
+                        color="white"
+                        padding="20px"
+                        borderRadius="1px"
+                        textAlign="center"
+                        backgroundColor="rgba(0,0,0,0.5)"
+                        position="absolute"
+                        top="46%"
+                        left={isNonMobile ? "10%" : "0%"}
+                        right={isNonMobile ? undefined : "0%"}
+                        margin={isNonMobile ? undefined : "0 auto"}
+                        maxWidth={isNonMobile ? undefined : "240px"}
+                    >
+                        <Typography color={shades.secondary[200]}>NEW ITEMS</Typography>
+                        <Typography variant="h1">Summer Sale</Typography>
+                        <Typography
+                            fontWeight="bold"
+                            color={shades.secondary[300]}
+                            sx={{ textDecoration: "underline" }}
+                        >
+                            Discover More
+                        </Typography>
+                    </Box>
+                </Box>
+            ))}
+        </Carousel>
+    );
 };
 
 export default TopCarousel;
